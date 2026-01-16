f379744903b83af4c6243ce03b81d84324f6d81b
Ambar Pichardo <ambarisioso@iMac-de-Ambar.local>
2026-01-16 14:15:59 -0600

    Fix: Export handlers for NextAuth API and create route

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import { authConfig } from './auth.config';
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [
         Credentials({
